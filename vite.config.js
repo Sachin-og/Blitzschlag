0572411dbd982ef12e8b96f09bbe0c4746c153bb
Sachin-og <131090877+Sachin-og@users.noreply.github.com>
2024-12-08 18:44:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blitzschlag/"
+  base: "/"
   // server: {
   //   host: '192.168.137.1',
   //   port: 3000,  // Or any other available port
